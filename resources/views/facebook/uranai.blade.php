f19363505508f573dc3bc4e4e2ae16531659d075
EC2 Default User <ec2-user@ip-172-31-25-83.us-east-2.compute.internal>
2020-08-02 00:41:46 +0000

    mM

---
+++ resources/views/facebook/uranai.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>テスト</title>
+        <link href="{{ secure_asset('css/uranai.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <img src="{{ asset('image/uranai_1.jpg') }}"></img>
+            <h1>あなたの人生、今で満足ですか...?</h1>
+            <h1>今の彼氏・旦那は、理想のパートナー?</h1>
+            <h1>ダイエットがうまくいかない。なぜ..?</h1>
+            <br>
+            <h3>・</h3>
+            <h3>・</h3>
+            <h3>・</h3>
+            <h3>悩みを抱えるだけじゃ解決しない。<br><span>占いの専門家</span>に相談しない...?<br><br><span>完全無料</span>で悩み解決の糸口に!?<br><br><span>心理テスト感覚</span>でOK！<br>初心者歓迎！</h3>
+            <br>
+            <br>
+        </header>
+        
+        <div class="header-sub">
+            <h2>抱える悩みを多角面から診断・分析！<br><span>日本一の呼び声高い</span>占い師集団！</h2>
+            <img src="{{ asset('image/uranai_2.jpg') }}"></img>
+        </div>
+        <div class="button">
+            <a href="https://preaf.jp/pa.do?s=v04031&o=47131&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
+            <p>※無料診断は公式アプリから...!</p>
+        </div>
+        <br>
+        
+        <!--<div class="body">-->
+        <!--    <h4>対面診断だと3ヶ月待ち!?<br><span>TVで有名なあの人</span>にオンラインで今すぐ診断してもらう!?</h4>-->
+        <!--    <img src="{{ asset('image/uranai_2.jpg') }}"></img>-->
+        <!--</div>-->
+        <!--<div class="button">-->
+        <!--    <a href="https://preaf.jp/pa.do?s=v04031&o=47131&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>-->
+        <!--</div>-->
+        
+        
+        <!--<img src="{{ asset('image/uranai_4.jpg') }}"></img>-->
+        <!--<br>-->
+        <!--<br>-->
+        <!--<br>-->
+        <!--<br>-->
+        <!--<br>-->
+        <!--<br>-->
+        <!--<br>-->
+        <!--<br>-->
+    </body>
+</html>
