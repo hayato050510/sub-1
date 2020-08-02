92344226cfd10f361873e3a6ee1395efe8b6b63a
EC2 Default User <ec2-user@ip-172-31-25-83.us-east-2.compute.internal>
2020-08-02 01:03:30 +0000

    mm

--- resources/views/facebook/uranai.blade.php
+++ resources/views/facebook/uranai.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>テスト</title>
+        <title>【無料】日本一当たる!? 占いアプリとは！</title>
         <link href="{{ secure_asset('css/uranai.css') }}" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     </head>
