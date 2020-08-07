08c16fa82f2ca2376946ee8b3197ea8e1aef01d2
EC2 Default User <ec2-user@ip-172-31-25-83.us-east-2.compute.internal>
2020-08-07 00:25:02 +0000

    Mm

--- resources/views/facebook/uranai.blade.php
+++ resources/views/facebook/uranai.blade.php
@@ -11,38 +11,41 @@
     <body>
         <header>
             <img src="{{ asset('image/uranai_1.jpg') }}"></img>
-            <h1>あなたの人生、<br>今で満足ですか...?</h1>
-            <br>
-            <br>
-            <h1>今の彼氏・旦那は、<br>本当に理想のパートナー?</h1>
-            <br>
-            <br>
-            <h1>ダイエットまた失敗...。<br>頑張ってるのになぜ...?</h1>
-            <br>
-            <h3>・</h3>
-            <h3>・</h3>
-            <h3>・</h3>
-            <h3>悩みを抱えるだけじゃ解決しない。<br><span>占いの専門家</span>に相談しない...?<br><br><span>完全無料</span>で悩み解決の糸口に!?<br><br><span>心理テスト感覚</span>でOK！<br>初心者歓迎！</h3>
-            <br>
+            <!--<h1>あなたの人生、<br>今で満足ですか...?</h1>-->
+            <!--<br>-->
+            <!--<br>-->
+            <!--<h1>今の彼氏・旦那は、<br>本当に理想のパートナー?</h1>-->
+            <!--<br>-->
+            <!--<br>-->
+            <!--<h1>ダイエットまた失敗...。<br>頑張ってるのになぜ...?</h1>-->
+            <!--<br>-->
+            <!--<h3>・</h3>-->
+            <!--<h3>・</h3>-->
+            <!--<h3>・</h3>-->
+            <!--<h3>悩みを抱えるだけじゃ解決しない。<br><span>占いの専門家</span>に相談しない...?<br><br><span>完全無料</span>で悩み解決の糸口に!?<br><br><span>心理テスト感覚</span>でOK！<br>初心者歓迎！</h3>-->
+            <!--<br>-->
+            <div class="header-sub">
+                <h2>抱える悩みを多角面から診断・分析！<br><span>統計に基づいた『深層心理診断』</span>も！</h2>
+            </div>
             <div class="button">
                 <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
-                <p>※無料診断は公式アプリから...!</p>
+                <p><i class="far fa-hand-point-up"></i> 心理テスト感覚で気軽に始めよう！</p>
             </div>
             <br>
             <br>
         </header>
         
-        <div class="header-sub">
-            <h2>抱える悩みを多角面から診断・分析！<br><span>日本一の呼び声高い</span>占い師集団！</h2>
-            <img src="{{ asset('image/uranai_2.jpg') }}"></img>
-        </div>
-        <div class="button">
-            <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
-            <p>※無料診断は公式アプリから...!</p>
-        </div>
-        <br>
+        <!--<div class="header-sub">-->
+        <!--    <h2>抱える悩みを多角面から診断・分析！<br><span>日本一の呼び声高い</span>占い師集団！</h2>-->
+        <!--    <img src="{{ asset('image/uranai_2.jpg') }}"></img>-->
+        <!--</div>-->
+        <!--<div class="button">-->
+        <!--    <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>-->
+        <!--    <p>※無料診断は公式アプリから...!</p>-->
+        <!--</div>-->
+        <!--<br>-->
         
-        <img src="{{ asset('image/lp_job_footer.jpg') }}"></img>
+        <!--<img src="{{ asset('image/lp_job_footer.jpg') }}"></img>-->
         <!--<div class="body">-->
         <!--    <h4>対面診断だと3ヶ月待ち!?<br><span>TVで有名なあの人</span>にオンラインで今すぐ診断してもらう!?</h4>-->
         <!--    <img src="{{ asset('image/uranai_2.jpg') }}"></img>-->
