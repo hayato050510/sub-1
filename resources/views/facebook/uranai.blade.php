259aa76137909fffe534ab84ec85600d756fd66e
EC2 Default User <ec2-user@ip-172-31-25-83.us-east-2.compute.internal>
2020-08-07 00:31:33 +0000

    mm

--- resources/views/facebook/uranai.blade.php
+++ resources/views/facebook/uranai.blade.php
@@ -10,7 +10,10 @@
     </head>
     <body>
         <header>
-            <img src="{{ asset('image/uranai_1.jpg') }}"></img>
+            <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank"><img src="{{ asset('image/uranai_1.jpg') }}"></img></a>
+            <div class="header-sub">
+                <h2>抱える悩みを<br>多角面から診断・分析！</h2>
+            </div>
             <div class="button">
                 <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
                 <p><i class="far fa-hand-point-up"></i> 心理テスト感覚で気軽に始めよう！</p>
@@ -28,8 +31,10 @@
             <!--<h3>・</h3>-->
             <!--<h3>悩みを抱えるだけじゃ解決しない。<br><span>占いの専門家</span>に相談しない...?<br><br><span>完全無料</span>で悩み解決の糸口に!?<br><br><span>心理テスト感覚</span>でOK！<br>初心者歓迎！</h3>-->
             <!--<br>-->
+            <br>
+            <img src="{{ asset('image/uranai_2.jpg') }}"></img>
             <div class="header-sub">
-                <h2>抱える悩みを<br>多角面から診断・分析！<br><span>統計に基づいた<br>『深層心理診断』</span>も！</h2>
+                <h2>統計に基づいた<br><span>『深層心理診断』</span>も！</h2>
             </div>
             <div class="button">
                 <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
@@ -41,7 +46,7 @@
         
         <!--<div class="header-sub">-->
         <!--    <h2>抱える悩みを多角面から診断・分析！<br><span>日本一の呼び声高い</span>占い師集団！</h2>-->
-        <!--    <img src="{{ asset('image/uranai_2.jpg') }}"></img>-->
+            <!--<img src="{{ asset('image/uranai_2.jpg') }}"></img>-->
         <!--</div>-->
         <!--<div class="button">-->
         <!--    <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>-->
