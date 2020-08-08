fad0efd19ce5c6ab8e5ddc7d3b7a44fad94f40b2
EC2 Default User <ec2-user@ip-172-31-25-83.us-east-2.compute.internal>
2020-08-08 06:23:15 +0000

    mm

--- resources/views/facebook/uranai.blade.php
+++ resources/views/facebook/uranai.blade.php
@@ -10,12 +10,12 @@
     </head>
     <body>
         <header>
-            <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank"><img src="{{ asset('image/uranai_1.jpg') }}"></img></a>
+            <a href="https://preaf.jp/pa.do?s=v04031&o=46779&guid=ON" target="_blank"><img src="{{ asset('image/lismy_1.jpg') }}"></img></a>
             <div class="header-sub">
                 <h2>抱える悩みを<br>多角面から診断・分析！</h2>
             </div>
             <div class="button">
-                <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
+                <a href="https://preaf.jp/pa.do?s=v04031&o=46779&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
                 <p><i class="far fa-hand-point-up"></i> 心理テスト感覚で気軽に始めよう！</p>
             </div>
             <!--<h1>あなたの人生、<br>今で満足ですか...?</h1>-->
@@ -32,12 +32,12 @@
             <!--<h3>悩みを抱えるだけじゃ解決しない。<br><span>占いの専門家</span>に相談しない...?<br><br><span>完全無料</span>で悩み解決の糸口に!?<br><br><span>心理テスト感覚</span>でOK！<br>初心者歓迎！</h3>-->
             <!--<br>-->
             <br>
-            <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank"><img src="{{ asset('image/uranai_2.jpg') }}"></img></a>
+            <a href="https://preaf.jp/pa.do?s=v04031&o=46779&guid=ON" target="_blank"><img src="{{ asset('image/lismy_2.jpg') }}"></img></a>
             <div class="header-sub">
                 <h2>統計に基づいた<br><span>『深層心理診断』</span>も！</h2>
             </div>
             <div class="button">
-                <a href="https://preaf.jp/pa.do?s=v04031&o=46142&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
+                <a href="https://preaf.jp/pa.do?s=v04031&o=46779&guid=ON" target="_blank" class="btn-square-shadow"><i class="fab fa-apple"></i> 公式AppStoreはコチラ <i class="fas fa-external-link-alt"></i></a>
                 <p><i class="far fa-hand-point-up"></i> 心理テスト感覚で気軽に始めよう！</p>
             </div>
             <!--<br>-->
